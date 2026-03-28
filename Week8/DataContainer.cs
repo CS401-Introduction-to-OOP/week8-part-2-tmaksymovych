3ee73e3ab3cc965929dd17b2000870eb5ca19be4
tymur <tmaksymovych@kse.org.ua>
2026-03-28 17:22:08 +0200

    created Generic Container

--- Week8/DataContainer.cs
+++ Week8/DataContainer.cs
@@ -1,6 +1,22 @@
 namespace Week8;
 
-public class DataContainer
+public class DataContainer<T> where T : DataItem
 {
-    
+    private readonly List<T> _items = new();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public long GetTotalSize()
+    {
+        long totalSize = 0;
+        foreach (var item in _items)
+        {
+            totalSize += item.GetSize();
+        }
+
+        return totalSize;
+    }
 }
