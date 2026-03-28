26137ede6a61d70aef4700b3a9089c7c3f6af918
tymur <tmaksymovych@kse.org.ua>
2026-03-28 15:56:33 +0200

    same logic as for file resource

--- Week8/NetworkResource.cs
+++ Week8/NetworkResource.cs
@@ -1,6 +1,20 @@
 namespace Week8;
 
-public class NetworkResource
+public class NetworkResource : Resource, IDisposable
 {
+    public NetworkResource(string name) : base(name){}
     
+    public override void Open()
+    {
+        IsOpen = true;
+    }
+    public override void Close()
+    {
+        IsOpen = false;
+    }
+    public void Dispose()
+    {
+        Dispose();
+        Console.WriteLine("Disposing in progress");
+    }
 }
