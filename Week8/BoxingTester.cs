f3201558cad6b7579760baa4ecabaa6d55403ab3
tymur <tmaksymovych@kse.org.ua>
2026-03-28 16:22:00 +0200

    Boxing

--- Week8/BoxingTester.cs
+++ Week8/BoxingTester.cs
@@ -1,6 +1,17 @@
 namespace Week8;
 
-public class BoxingTester
+public static class BoxingTester
 {
-    
+    public static void Test()
+    {
+        int a = 10;
+        object b = a;
+        int c = (int)b;
+        
+        Console.WriteLine($"main : {a}");
+        Console.WriteLine($"Boxed : {b}");
+        Console.WriteLine($"Unboxed : {c}");
+        
+        // value does not change in a even though you change b, because it was boxed
+    }
 }
