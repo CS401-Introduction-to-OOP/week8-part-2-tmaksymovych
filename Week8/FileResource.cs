dc2694e74c5010d3a9aa238112ea464abf10f786
tymur <tmaksymovych@kse.org.ua>
2026-03-28 15:54:13 +0200

    created fileresource file. used Idisposable

--- Week8/FileResource.cs
+++ Week8/FileResource.cs
@@ -1,6 +1,20 @@
 namespace Week8;
 
-public class FileResource
+public class FileResource : Resource, IDisposable
 {
+    public FileResource(string name) : base(name){}
     
+    public override void Open()
+    {
+        IsOpen = true;
+    }
+    public override void Close()
+    {
+        IsOpen = false;
+    }
+    public void Dispose()
+    {
+        Dispose();
+        Console.WriteLine("Disposing in progress");
+    }
 }
