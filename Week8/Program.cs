1f88c623877287aac12915025e1bed3d689f717c
tymur <tmaksymovych@kse.org.ua>
2026-03-28 16:08:40 +0200

    end

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,22 @@
-﻿
+﻿namespace Week8;
+
 class Program
 {
     public static void Main()
     {
+        var file = new FileResource("report.txt");
+        var network = new NetworkResource("api.company.local");
+        var manager = new ResourceManager<Resource>();
         
+        manager.AddResource(file);
+        manager.AddResource(network);
+        manager.OpenAll();
+
+        using (var fl = new FileResource("data.csv"))
+        {
+            fl.Open();
+        }
+        manager.CloseAll();
     }
     
 }
